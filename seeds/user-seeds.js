bd21e47f79cef5c00821c4101513402cc6420194
jve <joe.veyhl@gmail.com>
2023-01-29 11:31:39 -0500

    fixed typo name -> username

--- seeds/user-seeds.js
+++ seeds/user-seeds.js
@@ -2,42 +2,42 @@ const { User } = require("../models");
 
 const userData = [
   {
-    name: "nancy_gauthier",
+    username: "nancy_gauthier",
     twitter: "nancyg",
     github: "nancyg",
     email: "nancy_g@test.com",
     password: "password1",
   },
   {
-    name: "martin_leblanc",
+    username: "martin_leblanc",
     twitter: "martinl",
     github: "martinl",
     email: "martin_l@test.com",
     password: "password2",
   },
   {
-    name: "zach_roy",
+    username: "zach_roy",
     twitter: "zachr",
     github: "zachr",
     email: "zach_r@test.com",
     password: "password3",
   },
   {
-    name: "joe_veyhl",
+    username: "joe_veyhl",
     twitter: "joev",
     github: "joev",
     email: "joe_v@test.com",
     password: "password4",
   },
   {
-    name: "dan_feingold",
+    username: "dan_feingold",
     twitter: "danf",
     github: "danf",
     email: "dan_f@test.com",
     password: "password5",
   },
   {
-    name: "anne_jang",
+    username: "anne_jang",
     twitter: "annej",
     github: "annej",
     email: "anne_j@test.com",
