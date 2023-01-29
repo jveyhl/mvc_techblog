d3c3020ad2e6447d8e5aa95fcee2a5f2b2373033
jve <joe.veyhl@gmail.com>
2023-01-29 11:33:17 -0500

    fixed typo content -> comment_text

--- seeds/comment-seeds.js
+++ seeds/comment-seeds.js
@@ -4,42 +4,42 @@ const commentData = [
   {
     user_id: 1,
     post_id: 5,
-    content: "This is GREAT!",
+    comment_text: "This is GREAT!",
   },
   {
     user_id: 4,
     post_id: 4,
-    content: "Whoa, excellent job!",
+    comment_text: "Whoa, excellent job!",
   },
   {
     user_id: 1,
     post_id: 4,
-    content: "Wicked! Much thanks to everyone!",
+    comment_text: "Wicked! Much thanks to everyone!",
   },
   {
     user_id: 3,
     post_id: 5,
-    content: "So many subscribers! Amazing!",
+    comment_text: "So many subscribers! Amazing!",
   },
   {
     user_id: 3,
     post_id: 2,
-    content: "So happy about this!",
+    comment_text: "So happy about this!",
   },
   {
     user_id: 3,
     post_id: 4,
-    content: "Been waiting for this! Keep up the good work!",
+    comment_text: "Been waiting for this! Keep up the good work!",
   },
   {
     user_id: 5,
     post_id: 3,
-    content: "This tool helped me so much!",
+    comment_text: "This tool helped me so much!",
   },
   {
     user_id: 2,
     post_id: 1,
-    content: "Awesome!",
+    comment_text: "Awesome!",
   },
 ];
 
