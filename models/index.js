3fad3be8ad12e14a0b9fe219795e463883ce883a
jve <joe.veyhl@gmail.com>
2023-02-19 17:58:57 -0500

    added Delete functionality

--- models/index.js
+++ models/index.js
@@ -1,35 +1,40 @@
-const User = require('./User');
-const Post = require('./Post');
-const Comment = require('./Comment');
+const User = require("./User");
+const Post = require("./Post");
+const Comment = require("./Comment");
 
 // Users can have multiple posts
 User.hasMany(Post, {
-    foreignKey: 'user_id'
+  foreignKey: "user_id",
 });
 
 // Posts only belong to one user
 Post.belongsTo(User, {
-    foreignKey: 'user_id',
+  foreignKey: "user_id",
+  onDelete: "CASCADE",
 });
 
 // Each comment can only belong to one user
 Comment.belongsTo(User, {
-    foreignKey: 'user_id'
-  });
+  foreignKey: "user_id",
+  onDelete: "CASCADE",
+});
 
 // Each comment can only belong to one post
 Comment.belongsTo(Post, {
-    foreignKey: 'post_id'
+  foreignKey: "post_id",
+  onDelete: "CASCADE",
 });
 
 // Users can have many comments
 User.hasMany(Comment, {
-    foreignKey: 'user_id'
+  foreignKey: "user_id",
+  onDelete: "CASCADE",
 });
 
 // Each post can have multiple comments
 Post.hasMany(Comment, {
-    foreignKey: 'post_id'
+  foreignKey: "post_id",
+  onDelete: "CASCADE",
 });
 
-module.exports = {User, Post, Comment};
+module.exports = { User, Post, Comment };
